a3db2ab9664fda706ef159f085f1792dd006fe1d
Hariom Singh <hariomsingh28453@gmail.com>
2026-09-18 12:13:38 +0530

    Remove accidentally duplicated nested folder

--- Wanderlust-main/utils/ExpressError.js
+++
@@ -1,8 +0,0 @@
-class ExpressError extends Error {
-  constructor(statusCode, message) {
-    super(message); // ✅ Pass the message to the built-in Error class
-    this.statusCode = statusCode;
-  }
-}
-
-module.exports = ExpressError;
